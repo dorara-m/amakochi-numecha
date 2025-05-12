5a3d7a4d5e404cb7df109a852e0fcac074158422
Shimon Dotera <dotera.s@tam-tam.co.jp>
2025-05-12 15:37:36 +0900

    fix: ogp

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/favicon.ico" />
-        <meta property="og:image" content="/ogp.png" />
+        <meta property="og:image" content="/ogp.jpg" />
         <meta property="og:site_name" content="雨東風ぬめちゃ" />
         <meta property="og:locale" content="ja_JP" />
       </head>
